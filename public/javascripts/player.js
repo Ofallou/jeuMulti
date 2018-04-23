eb00140d608431128ff7cf0a3d717966f28236b0
oumzo <ofbk@hotmail.fr>
2018-04-24 01:21:18 +0200

    Jeu multijoueur version 1.1

---
+++ public/javascripts/player.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const objectId = require('mongodb').ObjectID;
+const pl = require('../../Schema/enregistrement');
+
+
+var getPlayer = function (pseudo) {
+
+    mongoose.connect('mongodb://localhost/quizz');
+
+    console.log('marche ou pas ???');
+    const player = mongoose.model('player', pl);
+    player.find({pseudo: pseudo}, function (err, data) {
+
+        console.log(err);
+
+
+    });
+
+
+};
+
+module.exports = getPlayer;
